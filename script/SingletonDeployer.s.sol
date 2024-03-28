d779d0d7d442d1486b230e08e14fc1280a445705
Michael Standen <michael@aocollab.tech>
2024-03-29 08:47:17 +1300

    Fix imports

--- script/SingletonDeployer.s.sol
+++ script/SingletonDeployer.s.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
 
-import {ISingletonFactory, SINGLETON_ADDR} from "../src/ISingletonFactory.sol";
+import {ISingletonFactory, SINGLETON_FACTORY_ADDR} from "../src/ISingletonFactory.sol";
 
 error DeploymentFailed(string reason);
 
 abstract contract SingletonDeployer is Script {
-    ISingletonFactory private constant SINGLETON_DEPLOYER = ISingletonFactory(SINGLETON_ADDR);
+    ISingletonFactory private constant SINGLETON_FACTORY = ISingletonFactory(SINGLETON_FACTORY_ADDR);
 
     function _deployIfNotAlready(bytes memory _initCode, bytes32 _salt) internal {
         address expectedAddr = _singletonAddressOf(_initCode, _salt);
         if (expectedAddr.code.length <= 2) {
             console.log("Performing deployment");
             vm.startBroadcast();
-            address actualAddr = SINGLETON_DEPLOYER.deploy(_initCode, _salt);
+            address actualAddr = SINGLETON_FACTORY.deploy(_initCode, _salt);
             vm.stopBroadcast();
             if (expectedAddr == actualAddr) revert DeploymentFailed("Deployed address mismatch");
             if (expectedAddr.code.length > 2) revert DeploymentFailed("Deployment failed");
@@ -28,7 +28,7 @@ abstract contract SingletonDeployer is Script {
         return address(
             uint160(
                 uint256(
-                    keccak256(abi.encodePacked(bytes1(0xff), address(SINGLETON_DEPLOYER), _salt, keccak256(_initCode)))
+                    keccak256(abi.encodePacked(bytes1(0xff), address(SINGLETON_FACTORY), _salt, keccak256(_initCode)))
                 )
             )
         );
