0efa61ff9dfd0d12de4ef9cd4cc00909a3dc258b
Michael Standen <michael@aocollab.tech>
2024-03-29 08:41:41 +1300

    Drop interface version

--- src/ISingletonFactory.sol
+++ src/ISingletonFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
 address constant SINGLETON_FACTORY_ADDR = 0xce0042B868300000d44A59004Da54A005ffdcf9f;
 address constant SINGLETON_EOA_ADDR = 0xBb6e024b9cFFACB947A71991E386681B1Cd1477D;
